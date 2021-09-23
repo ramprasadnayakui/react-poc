8545a63a8143d65f5dae19142f1faebc732fe0b7
ramprasadnayakui <ramprasadnayakui@gmail.com>
2021-09-23 22:30:16 +0530

    1 file modified

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,16 +3,20 @@ import { Button } from 'react-bootstrap';
 import "./Timer.css";
 
 export default function Timer(){
-  const [currentTime, setTime] = useState(new Date().toLocaleString());
+  const [currentCount, setCount] = useState(0);
+
+  function startCount(){
+    var interval = setInterval(() => {setCount(currentCount+1) }, 1000);
+  }
 
   return(
     <div className="Timer">
-    <div className="lander">
-      <Button>Start timer</Button>
-        <h4>Current time: {currentTime}</h4>
+      <div className="lander">
+        <Button onClick={startCount}>Start timer</Button>
+        <h4>Current time: {currentCount}</h4>
         <Button >Stop timer</Button>
-    </div>
-  </div>
+      </div>
+   </div>
   )
   
 }
