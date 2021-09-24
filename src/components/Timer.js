15f14f7cb29f520d7c4e0bae81dd94ddf8680f45
ramprasadnayakui <ramprasadnayakui@gmail.com>
2021-09-24 18:01:18 +0530

    2 files modified

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,18 +3,27 @@ import { Button } from 'react-bootstrap';
 import "./Timer.css";
 
 export default function Timer(){
-  const [currentCount, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [isPaused, setPause] = useState(true);
+  const [intervalId, setIntervalId] = useState(0)
 
-  function startCount(){
-    var interval = setInterval(() => {setCount(currentCount+1) }, 1000);
-  }
+  const startCount = () => {
+    var intervalId = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000)
+    setIntervalId(intervalId);
+}
+
+const stopCount = () => {
+  clearInterval(intervalId);
+}
 
   return(
     <div className="Timer">
       <div className="lander">
-        <Button onClick={startCount}>Start timer</Button>
-        <h4>Current time: {currentCount}</h4>
-        <Button >Stop timer</Button>
+        <Button className="startCount" onClick={startCount}>Start</Button>
+        <h4>Current time: {count}</h4>
+        <Button className="stopCount" onClick={stopCount}>Stop</Button>
       </div>
    </div>
   )
